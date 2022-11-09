1c3bf825b5f8e1b438ace380bb458335e4468aa3
Makayla <makaylawms0426@gmail.com>
2022-11-09 11:56:06 -0800

    Have created user input for the place

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,21 @@ public class Main
     {
         Mad queen = new Mad();
         String temp;
-
-        System.out.println("Please insert a thing (if a word using only letters all lowercase is not entered \"bugs\" will be the default)");
         Scanner scanner = new Scanner(System.in);
+
+        //user input the thing
+        System.out.println("Please insert a thing (if a word using only letters all lowercase is not entered \"bugs\" will be the default)");
         temp = scanner.next();
         queen.setThing(temp);
+        //get rid of this line later
         System.out.println(queen.getThing());
+
+        //user input the place
+        System.out.println("Please insert a place (if a word using only letters with an uppercase first letter is not entered \"Narnia\" will be the default");
+        temp = scanner.next();
+        queen.setPlace(temp);
+        //get rid of this line later
+        System.out.println(queen.getPlace());
+
     }//end of main method
 }//end of Main class
